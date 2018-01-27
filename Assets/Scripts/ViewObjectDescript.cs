f5e28efe571954ed006c0d582d1adf6155c4c0a3
armanamjad <arman.amjad02@gmail.com>
2018-01-27 14:10:52 -0800

    Added scripts for load scene and view object description
    load scene - loads DM view scene when start button is pressed
    view object description - object description text appears when object
    is clicked in DM view

---
+++ Assets/Scripts/ViewObjectDescript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewObjectDescript : MonoBehaviour {
+	private GUIStyle descriptStyle = new GUIStyle();
+	private bool clickedObj = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void showDescription (string descript){
+		descriptStyle.fontSize = 40;
+		GUI.Label(new Rect(Screen.height/2 - 20, Screen.height/2 - 20,100,100), descript, descriptStyle);
+	}
+		
+	void OnGUI(){
+		if (clickedObj) {
+			showDescription ("This is a test");
+		}
+	}
+
+	void OnMouseOver(){
+		if (Input.GetMouseButtonDown (0)) {
+			clickedObj = !clickedObj;
+		}
+	}
+	/*
+	void OnMouseExit(){
+			clickedObj = false;
+	}
+	*/
+}
