5b926ee4f97709bd4aeb0e5d7d488db005ff85f2
Nathan Krueger <kruegern@uci.edu>
2018-01-28 09:06:54 -0800

    Finished map, report if any holes; added audio integration for footsteps; fixed collisions; player movement is smoother

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour {
 
     public float movementSpeed = 1.0f;
+    private Vector3 moveDirection = Vector3.zero;
     public float mouseSpeed = 0.0f;
-    public bool fog = true;
+    public bool fogOn = true;
     public Color fogColor = Color.black;
     public float fogDensity = 0.2f;
 
-	// Use this for initialization
-	void Start () {
+    public AudioClip[] walking;
+    private int i = 0;
+    public AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
         RenderSettings.fogColor = fogColor;
         RenderSettings.fog = true;
         RenderSettings.fogDensity = fogDensity;
@@ -20,10 +25,33 @@ public class Player_Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 dir = new Vector3(); //(0,0,0)
+        ++i;
+        if (i >= walking.Length)
+            i = 0;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fog = fogOn;
+        RenderSettings.fogDensity = fogDensity;
+        RenderSettings.fogMode = FogMode.Exponential;
+
+        float dx = 0.0f;
+        float dz = 0.0f;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            dz = 1.0f;
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            dx = -1.0f;
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            dz = -1.0f;
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            dx = 1.0f;
+
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(dx, 0, dz);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= movementSpeed;
+        controller.Move(moveDirection * Time.deltaTime);
         //float CharacterSpeed = 10.0f;
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        /*if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             dir.z += 1.0f;
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             dir.x -= 1.0f;
@@ -35,14 +63,19 @@ public class Player_Move : MonoBehaviour {
             dir.y -= 1.0f;
         else if (Input.GetKey(KeyCode.Space))
             dir.y += 1.0f;
-
-        dir.Normalize();
-        transform.Translate(dir * movementSpeed * Time.deltaTime);
+            */
+        //moveDirection.Normalize();
+        //transform.Translate(moveDirection * movementSpeed * Time.deltaTime);
         float x = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSpeed;
         //float z = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSpeed;
         
         //transform.position += new Vector3(dx * Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad), 0.0f, dz * Mathf.Sin(transform.rotation.eulerAngles.y * Mathf.Deg2Rad));
         transform.Rotate(0, x, 0);
+        if ((x != 0.0f || moveDirection.x != 0.0f || moveDirection.z != 0.0f) && !audio.isPlaying)
+        {
+            audio.clip = walking[i];
+            audio.Play();
+        }
         //transform.Rotate(-z, 0, 0);
-	}
+    }
 }
